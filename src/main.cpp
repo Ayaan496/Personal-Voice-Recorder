8ed1f0536911015cf6f68d4e94053f35340bd7db
Ayaan496 <mushtaqa@vcu.edu>
2026-03-14 16:54:29 -0400

    Added comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+//Libraries
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -17,59 +18,53 @@
 #define SAMPLE_RATE 16000
 #define BUFFER_SIZE 512
 
+
+//Variables
 bool lastButtonState = HIGH;
-unsigned long lastDebounceTime = 0;
-
-
-
-
 Adafruit_SSD1306 display(128,64,&Wire, -1);
-
-
-
 bool isRecording = false;
 int recordingSeconds = 0;
 unsigned long lastSecondTick = 0;
-
 int32_t audioBuffer[BUFFER_SIZE];
 
 
-// put function declarations here:
 
 
+//I2S setup
 void setupI2S(){
+  //config struct
+  i2s_config_t config = {
+    .mode                 = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),//Recieve only
+    .sample_rate          = SAMPLE_RATE,
+    .bits_per_sample      = I2S_BITS_PER_SAMPLE_32BIT,
+    .channel_format       = I2S_CHANNEL_FMT_RIGHT_LEFT,//Stereo, switch to _ONLY_LEFT for mono
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .intr_alloc_flags     = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count        = 4,
+    .dma_buf_len          = BUFFER_SIZE,
+    .use_apll             = false,
+    .tx_desc_auto_clear   = false,
+    .fixed_mclk           = 0
+  };
 
-i2s_config_t config = {
-  .mode                 = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-  .sample_rate          = SAMPLE_RATE,
-  .bits_per_sample      = I2S_BITS_PER_SAMPLE_32BIT,
-  .channel_format       = I2S_CHANNEL_FMT_RIGHT_LEFT,
-  .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-  .intr_alloc_flags     = ESP_INTR_FLAG_LEVEL1,
-  .dma_buf_count        = 4,
-  .dma_buf_len          = BUFFER_SIZE,
-  .use_apll             = false,
-  .tx_desc_auto_clear   = false,
-  .fixed_mclk           = 0
-};
+  i2s_pin_config_t pins = {
 
-i2s_pin_config_t pins = {
+    .bck_io_num = I2S_SCK_PIN,
+    .ws_io_num = I2S_WS_PIN,
+    .data_out_num = I2S_PIN_NO_CHANGE, //No data out
+    .data_in_num = I2S_SD_PIN
 
-  .bck_io_num = I2S_SCK_PIN,
-  .ws_io_num = I2S_WS_PIN,
-  .data_out_num = I2S_PIN_NO_CHANGE,
-  .data_in_num = I2S_SD_PIN
+  };
 
-};
-
-i2s_driver_install(I2S_PORT, &config, 0, NULL);
-i2s_set_pin(I2S_PORT, &pins);
-i2s_start(I2S_PORT);
+  //I2s activation
+  i2s_driver_install(I2S_PORT, &config, 0, NULL);
+  i2s_set_pin(I2S_PORT, &pins);
+  i2s_start(I2S_PORT);
 
 }
 
 
-
+//display idle screen
 void showIdle(){
 
   display.clearDisplay();
@@ -86,7 +81,7 @@ void showIdle(){
   display.display();
 }
 
-
+//display recording screen
 void showRecording(int seconds) {
   display.clearDisplay();
   display.setTextSize(1);
@@ -96,7 +91,7 @@ void showRecording(int seconds) {
   display.drawLine(0, 12, 128, 12, SSD1306_WHITE);
 
     // Flashing REC indicator
-  if ((millis() / 500) % 2 == 0) {
+  if ((millis() / 500) % 2 == 0) { //fashing circle every half second
     display.fillCircle(6, 28, 5, SSD1306_WHITE);
   }
   display.setTextSize(2);
@@ -113,7 +108,7 @@ void showRecording(int seconds) {
   display.println("Click again to stop");
   display.display();
 }
-
+//display saved screen
 void showSaved(int seconds) {
   display.clearDisplay();
   display.setTextSize(1);
@@ -138,43 +133,45 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   Wire.begin(SDA_PIN, SCL_PIN);
 
-
+  //start the display and check if its findable
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
     
     Serial.println("Display not found");
     while(true);
   }
 
- setupI2S();
-
-delay(100);
-lastSecondTick = millis();
-showIdle();
+  setupI2S();
+  delay(100);
+  lastSecondTick = millis();
+  showIdle();
  
 
 }
 
+//send audio data over serial
 void sendAudioOverSerial(int32_t* buffer, size_t samples){
   Serial.write((uint8_t*)buffer, samples * sizeof(int32_t));
 }
 
 void loop() {
+
+  //Button logic
   bool currentButtonState = digitalRead(BUTTON_PIN);
 
   if (currentButtonState == LOW && lastButtonState == HIGH) {
     lastButtonState = currentButtonState;
-    delay(50); // simple debounce
+    delay(50); //debounce
 
-    // confirm it's still pressed after debounce
+    //if button is pressed and not recording, start recording and start counting time
     if (digitalRead(BUTTON_PIN) == LOW) {
       if (!isRecording) {
         isRecording = true;
         recordingSeconds = 0;
         lastSecondTick = millis();
         
-        Serial.write("START");
+        Serial.write("START");//send to python script
         showRecording(0);
-      } else {
+      } else { //if button pressed and you are recording, stop recording
         isRecording = false;
         Serial.write("STOP");
         showSaved(recordingSeconds);
@@ -186,16 +183,17 @@ void loop() {
 
   lastButtonState = currentButtonState;
 
+  //take data from dma buffer and put it into audio buffer
   if (isRecording) {
     size_t bytesRead = 0;
     i2s_read(I2S_PORT, audioBuffer, sizeof(audioBuffer), &bytesRead, pdMS_TO_TICKS(10));
-
+    //if data was read, send the data over serial
      if (bytesRead > 0) {
       sendAudioOverSerial(audioBuffer, bytesRead / sizeof(int32_t));
     }
 
 
-
+    //increment recording seconds every second
     if (millis() - lastSecondTick >= 1000) {
       recordingSeconds++;
       lastSecondTick = millis();
